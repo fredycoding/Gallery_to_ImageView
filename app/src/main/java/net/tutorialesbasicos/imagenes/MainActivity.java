c55e5977df33f52a73b364ef60574e42fd266e7b
xbox360colombia <xbox360colombia@gmail.com>
2018-09-16 19:21:33 -0500

    Initial commit

---
+++ app/src/main/java/net/tutorialesbasicos/imagenes/MainActivity.java
@@ -0,0 +1,43 @@
+package net.tutorialesbasicos.imagenes;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE = 1;
+    ImageView foto_gallery;
+    Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        foto_gallery = (ImageView)findViewById(R.id.foto_gallery);
+
+        foto_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            foto_gallery.setImageURI(imageUri);
+        }
+    }
+}
